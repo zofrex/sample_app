bffc6be8badec4186c7fd17dca9bb21abcaf2a8b
zofrex <zofrex@gmail.com>
2012-04-29 20:40:40 +0100

    Change title tests to select h1 tags

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,7 @@ describe "Static pages" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the title 'Home'" do
@@ -18,7 +18,7 @@ describe "Static pages" do
   describe "Help page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the title 'Help'" do
@@ -30,11 +30,11 @@ describe "Static pages" do
   describe "About page" do
     it "should have the content 'About US'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the title 'About'" do
-      visit '/static_pages/help'
+      visit '/static_pages/about'
       page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About")
     end
   end
