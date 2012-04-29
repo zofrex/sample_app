c177a536af808f1756c35d137baf45f5e944bf02
zofrex <zofrex@gmail.com>
2012-04-29 21:07:36 +0100

    Add test for contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -40,4 +40,16 @@ describe "Static pages" do
       page.should have_selector('title', :text => "#{base_title} | About")
     end
   end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "#{base_title} | Contact")
+    end
+  end
 end
