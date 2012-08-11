5860081bec93c902951970287fb4a527a55b4b0a
zofrex <zofrex@gmail.com>
2012-08-11 18:30:03 +0100

    Add password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,8 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, email_format: true, uniqueness: { case_sensitive: false }
