60d6a53c0452dbb5574083192b8bf123c6710e66
zofrex <zofrex@gmail.com>
2012-08-11 18:26:24 +0100

    Add more tests for password authentication and min length

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,6 +22,7 @@ describe User do
   it { should respond_to :password_digest }
   it { should respond_to :password }
   it { should respond_to :password_confirm }
+  it { should respond_to :authenticate }
 
   it { should be_valid }
 
@@ -128,5 +129,24 @@ describe User do
   describe "when password confirmation is nil" do
     before { @user.password_confirmation = nil }
     it { should_not be_valid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by_email(@user.email) }
+
+    describe "with valid password" do
+      it { should == found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+      specify { user_for_invalid_password.should be_false }
+    end
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a"*5 }
+    it { should be_invalid }
   end
 end
