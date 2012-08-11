735a6fd4b9baa8c5eac6530aefd833189e9c7b5f
zofrex <zofrex@gmail.com>
2012-08-11 18:14:47 +0100

    Add test to check password and confirmation must match

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -119,4 +119,9 @@ describe User do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
   end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid}
+  end
 end
